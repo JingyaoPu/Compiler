0bf0fb8e39e41ea354209dc134d4f4096611503f
Jingyao Pu <nicpu@nicdemacbook-pro.local>
2019-01-30 21:14:05 -0800

    Initial Commit

---
+++ Compiler/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Compiler
+//
+//  Created by Nic Pu on 1/30/19.
+//  Copyright © 2019 Nic Pu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
